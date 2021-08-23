fd95da5fecaf84066b93c8b77aad5fa31ce5f84a
Kevin Rodriguez <87106659+Krcoded@users.noreply.github.com>
2021-08-23 11:46:29 -0400

    working fighters

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let width = 15;
 let direction = 1;
 let invadersId;
 let goingRight = true;
-let aliensRemoved = [];
+let sithsRemoved = [];
 let results = 0;
 
 for (let i = 0; i < 225; i++) {
@@ -15,15 +15,15 @@ for (let i = 0; i < 225; i++) {
 
 const squares = Array.from(document.querySelectorAll(".grid div"));
 
-const alienInvaders = [
+const sithInvaders = [
   0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 30, 31,
   32, 33, 34, 35, 36, 37, 38, 39,
 ];
 
 function draw() {
-  for (let i = 0; i < alienInvaders.length; i++) {
-    if (!aliensRemoved.includes(i)) {
-      squares[alienInvaders[i]].classList.add("invader");
+  for (let i = 0; i < sithInvaders.length; i++) {
+    if (!sithsRemoved.includes(i)) {
+      squares[sithInvaders[i]].classList.add("invader");
     }
   }
 }
@@ -31,8 +31,8 @@ function draw() {
 draw();
 
 function remove() {
-  for (let i = 0; i < alienInvaders.length; i++) {
-    squares[alienInvaders[i]].classList.remove("invader");
+  for (let i = 0; i < sithInvaders.length; i++) {
+    squares[sithInvaders[i]].classList.remove("invader");
   }
 }
 
@@ -53,29 +53,28 @@ function moveShooter(e) {
 document.addEventListener("keydown", moveShooter);
 
 function moveInvaders() {
-  const leftEdge = alienInvaders[0] % width === 0;
-  const rightEdge =
-    alienInvaders[alienInvaders.length - 1] % width === width - 1;
+  const leftEdge = sithInvaders[0] % width === 0;
+  const rightEdge = sithInvaders[sithInvaders.length - 1] % width === width - 1;
   remove();
 
   if (rightEdge && goingRight) {
-    for (let i = 0; i < alienInvaders.length; i++) {
-      alienInvaders[i] += width + 1;
+    for (let i = 0; i < sithInvaders.length; i++) {
+      sithInvaders[i] += width + 1;
       direction = -1;
       goingRight = false;
     }
   }
 
   if (leftEdge && !goingRight) {
-    for (let i = 0; i < alienInvaders.length; i++) {
-      alienInvaders[i] += width - 1;
+    for (let i = 0; i < sithInvaders.length; i++) {
+      sithInvaders[i] += width - 1;
       direction = 1;
       goingRight = true;
     }
   }
 
-  for (let i = 0; i < alienInvaders.length; i++) {
-    alienInvaders[i] += direction;
+  for (let i = 0; i < sithInvaders.length; i++) {
+    sithInvaders[i] += direction;
   }
 
   draw();
@@ -85,13 +84,13 @@ function moveInvaders() {
     clearInterval(invadersId);
   }
 
-  for (let i = 0; i < alienInvaders.length; i++) {
-    if (alienInvaders[i] > squares.length) {
+  for (let i = 0; i < sithInvaders.length; i++) {
+    if (sithInvaders[i] > squares.length) {
       resultsDisplay.innerHTML = "GAME OVER";
       clearInterval(invadersId);
     }
   }
-  if (aliensRemoved.length === alienInvaders.length) {
+  if (sithsRemoved.length === sithInvaders.length) {
     resultsDisplay.innerHTML = "YOU WIN";
     clearInterval(invadersId);
   }
@@ -117,8 +116,8 @@ function shoot(e) {
       );
       clearInterval(laserId);
 
-      const alienRemoved = alienInvaders.indexOf(currentLaserIndex);
-      aliensRemoved.push(alienRemoved);
+      const sithRemoved = sithInvaders.indexOf(currentLaserIndex);
+      sithsRemoved.push(sithRemoved);
       results++;
       resultsDisplay.innerHTML = results;
     }
